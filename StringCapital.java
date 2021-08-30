fa2e97ce9975b1ab23e6bafb1e8db0ab2d4ff6a5
Sparsh goyal <51365171+sparshgoyal2014@users.noreply.github.com>
2021-08-30 20:47:36 +0530

    Add files via upload

---
+++ StringCapital.java
@@ -0,0 +1,27 @@
+
+public class Main {  
+    
+    public static String capitalizeWord(String str){  
+    
+        String words[] = str.split("\\s");  
+        String capitalizeWord = "";  
+        
+        for(String w: words){  
+            
+            String first = w.substring(0,1);  
+
+            String afterfirst = w.substring(1);  
+            
+            capitalizeWord += first.toUpperCase() + afterfirst+" ";  
+        }  
+        
+        return capitalizeWord.trim();  
+    }
+
+
+    public static void main(String[] args){
+        String str = "hello, i am java."
+        System.out.println(capitalizeWord(str));
+    }
+
+}  
---
+++ separateOddEven.java
@@ -0,0 +1,39 @@
+public class Main {
+    
+    static void separate(Integer arr[], int n)
+    {
+        int l = 0, r = n - 1;
+ 
+        int k = 0;
+ 
+        while (l < r){
+         
+            while (arr[l] % 2 != 0){
+                l++;
+                k++;
+            }
+ 
+            while (arr[r] % 2 == 0 && l < r)
+                r--;
+ 
+            if (l < r) {             
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+        }
+ 
+        Arrays.sort(arr, 0, k); 
+        Arrays.sort(arr, k, n);
+    }
+ 
+    public static void main(String[] args){
+        Integer arr[] = { 1, 3, 2, 7, 5, 4 };
+ 
+        separate(arr, arr.length);
+ 
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
